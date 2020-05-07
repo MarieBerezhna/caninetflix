5547a32f06382ee05635afde9e108d8fba0022ef
weirdox <marie.berezhna@gmail.com>
2020-05-08 02:57:20 +0300

    move to popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,5 +1,40 @@
 $(document).ready(function () {
     $("#status").text("busy");
-    console.log("popup");
+    console.log("popup, " + Date.now());
 });
+const enableExtension = () => {
+    chrome.runtime.sendMessage({ 
+        command: 'enable', 
+        message: "start" 
+      });  
+  };
+
+let checkTabs = (tabs) => {
+  console.log(tabs);
+  if (tabs.length) {
+    for (var i = 0; i < tabs.length; i++) {
+      let title = tabs[i].title;
+      chrome.runtime.sendMessage({ 
+        message: "changeIcon", 
+        "newIconPath" : title === "Netflix"? "icons/red128.png" : "icons/green128.png" 
+      });
+    }
+  }
+};
+
+let requestTabs = () => {
+  console.log("here");
+  chrome.runtime.sendMessage({
+    message: "checkTabs"
+  }, (response) => {
+    debugger;
+    console.log(response.message);
+    let tabs = response.message;
+    checkTabs(tabs);
+  });
+};
+
+
+enableExtension();
+requestTabs();
 
